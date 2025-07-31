fd91402520c207c68e495c6a26250ecfa49cdd75
liam woodward <liamwoodward20@outlook.com>
2025-07-31 09:55:52 +0100

    updating javascript the invalidemail function

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
 const submitBtn = document.querySelector('.subscribe-btn');
 const emailInput = document.querySelector('#email-input')
 const invalidMessage = document.querySelector('.error-message')
+const newsContainer = document.querySelector('.news-card.hide')
+const showsuccess = document.querySelector('.success-card')
 
 
 function invalidEmail() {
-  if (emailInput.value === ''){
+  if (emailInput.value === 'example.user123@email.com'){
     emailInput.style.border = '1px solid #e74c3c'
     emailInput.style.color = '#e74c3c'
     invalidMessage.style.display = 'block'
@@ -13,7 +15,15 @@ function invalidEmail() {
   }
 }
 
+function validEmail() {
+  if (emailInput.value === 'email@company.com') {
+   
+  } else {
+    
+  }
+}
+
 submitBtn.addEventListener('click', function(){
   invalidEmail()
-  
+  validEmail()
 })
